f545bdfc30f0638953d7a82b4e0ec896e544a892
Kushal Patel <patelku@umich.edu>
2024-03-15 00:27:06 -0400

    break

--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -38,7 +38,7 @@ const Navbar = () => (
           LinkedIn
         </NavMenuItem>
         <NavMenuItem
-          href="/assets/images/Kushal_Patel_Resume.pdf"
+          href="/assets/images/Kushal_Patel_Resufdme.pdf"
           target="_blank"
         >
           Resume
