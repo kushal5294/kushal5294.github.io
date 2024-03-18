9fc8071be7a809044891992cc444df7d6fa4a030
Kushal Patel <patelku@umich.edu>
2024-03-17 20:23:07 -0400

    footer

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,6 +1,6 @@
-import { FooterCopyright, Section } from 'astro-boilerplate-components';
+import { FooterCopyright, Section } from "astro-boilerplate-components";
 
-import { AppConfig } from '@/utils/AppConfig';
+import { AppConfig } from "@/utils/AppConfig";
 
 const Footer = () => (
   <Section>
