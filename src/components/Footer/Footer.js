222874603821a9b0e5e347f753992c11d3fe15a5
Kushal Patel <patelku@umich.edu>
2024-08-16 01:31:57 -0400

    footer change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return (
-    <footer className="footer">
-      © 2024 Kushal Patel. All rights reserved.
-    </footer>
-  );
+  return <footer className="footer">© Kushal Patel 2024</footer>;
 }
 
 export default Footer;
