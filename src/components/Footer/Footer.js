a571401fb71c147e89e9953715a59fcafb12bd85
Kushal Patel <113399080+kushal5294@users.noreply.github.com>
2025-02-16 15:57:08 -0500

    nfl simulator interface

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <footer className="footer">© Kushal Patel 2024</footer>;
+  return <footer className="footer">© Kushal Patel 2025</footer>;
 }
 
 export default Footer;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Header.css";
 import profile from "../../assets/headshot.jpeg";
+import Michigan from "../../assets/mich.png";
+import Amazon from "../../assets/amzn.png";
 
 function Header() {
   return (
@@ -11,9 +13,16 @@ function Header() {
           <span className="gentle-hover-shake">👋</span>
         </h1>
         <p>
-          <span className="highlight">Junior</span> studying computer science at{" "}
-          <span className="highlight">The University of Michigan</span>
+          CS @ <span className="highlight">U of M</span>
         </p>
+        {/* <p>
+          SDE Intern @ <span className="highlight">Amazon</span>{" "}
+        </p> */}
+        <div>
+          <img className="blockM" src={Michigan}></img>
+          {/* <span className="highlight bar">|</span>
+          <img className="amzn" src={Amazon}></img> */}
+        </div>
       </div>
       <img src={profile} alt="headshot" className="profile-pic" />
     </header>
